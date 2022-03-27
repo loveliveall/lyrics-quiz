1ca77e3688a5aced70c97bc3709b64b836f97d9f
LoveLive <lovelivefam@gmail.com>
2022-03-27 16:10:26 +0900

    Introduce routing library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '@/pages';
 
 function App() {
   return (
-    <div>Hello, World!</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
